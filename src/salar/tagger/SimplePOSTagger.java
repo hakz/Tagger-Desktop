2c079ca7106744d26fc93133558f996f868c4f4e
Morteza Ansarinia <ansarinia@me.com>
2011-12-09 03:37:23 +0330

    Adds UI and completes pos tag extraction codes, plus presents output inside UI (SWT UI)

--- src/salar/tagger/SimplePOSTagger.java
+++ src/salar/tagger/SimplePOSTagger.java
@@ -1,39 +1,61 @@
 package salar.tagger;
 
-import opennlp.tools.cmdline.PerformanceMonitor;
 import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.tokenize.WhitespaceTokenizer;
-import opennlp.tools.util.ObjectStream;
-import opennlp.tools.util.PlainTextByLineStream;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SimplePOSTagger {
-	public static void main(String[] args) throws IOException {
+	
+	private static SimplePOSTagger instance = null;
+	public SimplePOSTagger() {
+		instance = null;
+	}
+
+	public static SimplePOSTagger getInstance() {
+		if (instance == null)
+			instance = new SimplePOSTagger();
+		return instance;
+	}
+	
+	public ArrayList<String> extractTags(String strDirPath) throws Exception {
+
+		ArrayList<String> results = new ArrayList<String>();
 		POSModel model = new POSModelLoader().load(new File("resources/en-pos-maxent.bin"));
-		PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
 		POSTaggerME tagger = new POSTaggerME(model);
 
-		String input = "I was sitting in my 5th grade geography class at my small school in Whitby, Ontario when our teacher told us there had been an attack in New York City, but he didn’t give us specifics on what had happened. He explained to us what terrorism was for the first time. Ironically, we had learned the day before about democracy, and how lucky we were to live in a safe country that was run by the people. My grandmother is American, so I saw no difference between the United States and Canada at the time- I hurt as deeply as any American did. I remember not understanding why someone would want to hurt Americans, and I worried for the rest of the day. We weren’t able to watch TV or see pictures until we left school, and I ran home past the houses of my neighbours, many of whom were crying audibly in their living rooms. When I got inside, my mother was on the (primitive) computer looking at pictures of the towers. She sat me down, and told me that a lot of people were missing, probably dead. She then told me that the CN Tower had been shut down, in case there was a similar attack on Toronto. Looking back, this day where I realized that living in North America was not a guarantee of security was truly the day my childhood ended.";
-		ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(input));
+		String[] filesPath = new File(strDirPath).list();
+		
+		if(filesPath == null || filesPath.length==0)
+			return null;
+		
+		Scanner lineScanner;
+		File txtFile;
+		for ( String txtFilePath : filesPath ) {
+			txtFile = new File(txtFilePath);
+			lineScanner = new Scanner(new FileReader(txtFile));
 
-		perfMon.start();
-		String line;
-		while ((line = lineStream.read()) != null) {
+			while (lineScanner.hasNextLine()) {
 
-			String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
-			String[] tags = tagger.tag(whitespaceTokenizerLine);
+				String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE.tokenize(lineScanner.nextLine());
+				String[] tags = tagger.tag(whitespaceTokenizerLine);
+				
+				POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
 
-			POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
-			System.out.println(sample.toString());
-
-			perfMon.incrementCounter();
+				String[] posTags = sample.getTags();
+				for (String tag:posTags)
+					results.add(tag);
+			}
+			lineScanner.close();
+			lineScanner = null;
 		}
-		perfMon.stopAndPrintFinalResult();
+
+		return results;
 	}
 }
--- src/salar/ui/MainWindow.java
+++ src/salar/ui/MainWindow.java
@@ -1,5 +1,7 @@
 package salar.ui;
 
+import java.util.ArrayList;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.layout.GridLayout;
@@ -7,15 +9,22 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+
+import salar.tagger.SimplePOSTagger;
 
 
 public class MainWindow {
 
 	protected Shell shell;
 	private Text txtDirectoryPath;
-	private Table table;
+	private Text text;
+	private Label lblDirectory;
 
 	public static void main(String[] args) {
 		try {
@@ -40,28 +49,50 @@ public class MainWindow {
 
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(450, 270);
 		shell.setText("Salar Word Tagger (Version 2.0)");
-		shell.setLayout(new GridLayout(1, false));
+		shell.setLayout(new GridLayout(3, false));
+		
+		lblDirectory = new Label(shell, SWT.NONE);
+		lblDirectory.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblDirectory.setText("Directory:");
 		
 		txtDirectoryPath = new Text(shell, SWT.BORDER);
-		txtDirectoryPath.setText("Data Directory");
 		txtDirectoryPath.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
+		text = new Text(shell, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
+		GridData gd_text = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
+		gd_text.heightHint = 170;
+		text.setLayoutData(gd_text);
+		
 		Button btnLoad = new Button(shell, SWT.NONE);
+		btnLoad.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				DirectoryDialog dialog = new DirectoryDialog (shell);
+				String platform = SWT.getPlatform();
+				dialog.setFilterPath (platform.equals("win32") || platform.equals("wpf") ? "c:\\" : "/");
+				String strSelectedDir = dialog.open ();
+				ArrayList<String> posTags;
+				try {
+					text.setText("");
+					posTags = SimplePOSTagger.getInstance().extractTags(strSelectedDir);
+					for (String str: posTags) {
+						text.append(str + System.getProperty("line.separator"));
+					}
+				} catch (Exception e) {
+					text.setText(e.toString());
+				}
+			}
+		});
+		btnLoad.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
 		btnLoad.setText("Load Data");
-
-		Button btnProcess = new Button(shell, SWT.NONE);
-		btnProcess.setText("Process Data");		
+				new Label(shell, SWT.NONE);
+				new Label(shell, SWT.NONE);
 		
-		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-		
-		ProgressBar progressBar = new ProgressBar(shell, SWT.NONE);
-		progressBar.setSelection(30);
-
+				Button btnProcess = new Button(shell, SWT.NONE);
+				btnProcess.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+				btnProcess.setText("Process Data");
 	}
 
 }
