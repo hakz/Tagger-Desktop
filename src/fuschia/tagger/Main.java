c0050c5479bd476b8f990dae3e7f2e335846fbaa
Morteza Ansarinia <ansarinia@me.com>
2012-05-02 17:51:35 +0430

    Simple construal tag counter for verbs and adjectives. outputs are at resources directories in CSV format

--- src/fuschia/tagger/Main.java
+++ src/fuschia/tagger/Main.java
@@ -1,10 +1,15 @@
 package fuschia.tagger;
 
-import fuschia.tagger.ui.*;
+import fuschia.tagger.ui.MainWindow;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
+						
+			//ConstrualProcessor construal = new ConstrualProcessor("/Volumes/Personal HD/Friends/Salar/");
+			//construal.start();
+			//while(construal.isAlive()){}
+			
 			MainWindow.getInstance().open();
 
 		} catch (Exception e) {
